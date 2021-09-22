cd3063ce9e913675079301a680d3d3d6bd2746f1
bangrija <90464337+bangrija@users.noreply.github.com>
2021-09-23 05:46:21 +0700

    Dasar, Variabel, dan Operator Pada PHP
    Dasar, Variabel, dan Operator Pada PHP

---
+++ praktikum2_2020573010020_rija/coba3_variabel2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $jalan="Medan - B.Aceh";
+    $noRumah=29;
+    $blok="4C";
+    $jumlahPenghuni=3;
+    echo"Hasil variabel variabel jalan +
+    variabel noRumah adalah";
+    echo $alamat=$jalan . $noRumah;
+    echo "<br>";
+    echo "Hasil variabel variabel noRumah 
+    + variabel jumlah penghuni adalah ";
+    echo $hasil=$noRumah + $jumlahPenghuni;
+    echo "<br>";
+    echo "Hasil variabel variabel blok +
+    variabel noRumah adalah";
+    echo $hasil = $blok + $noRumah;
+    echo "<br>";
+    ?>
+</body>
+</html>
