0667c0051dca7fba50d1be44ad604c968c7b04ab
bangrija <90464337+bangrija@users.noreply.github.com>
2021-12-20 11:38:07 +0700

    PRAKTIKUM 3

---
+++ Praktikum3/Praktikum3_2020573010020_rija/(2)percabangan_if_elseif_else.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $date=("D");
+    if ($date=="Sat")
+    echo "Selamat Berakhir Pekan!";
+    elseif($date=="Fri")
+    echo "Selamat Menunaikan Sholat Jumat Bagi Yang Muslim";
+    else
+    echo "Selamat Belajar";
+    ?>
+</body>
+</html>
